8ddeea96eda120b24ccbe90de24bba45dcbd8072
Modestas Kižauskas <modestas@kizauskas.lt>
2021-03-29 17:11:55 +0300

    adding function trigger

--- replay_tournament_rankings/scripts.js
+++ replay_tournament_rankings/scripts.js
@@ -1,1 +1,3 @@
-$('p').css({border: '5px solid red', color: 'green', padding: '20px'});
+jQuery(document).ready(function(){
+    $('p').css({border: '5px solid red', color: 'green', padding: '20px'});
+});
